0bca7c6dbdaf19fd987ee2fef5efc442cbf5eda0
H3xept <h3xept@gmail.com>
2016-12-16 18:31:53 +0100

    [*] Test structure

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -11,22 +11,22 @@
 
     </head>
     <body>
-        <div class="container-fluid">
+        <div class="container fluid">
             <?php $name = "Orazio Grinzosi"; $class = "5L"; ?>
             @include('partials._header')  
         </div>
-        <div class="row">
-            <div class="col-md-4 col-sm-12">
-                @yield('navigation')
-            </div>
-            <div class="col-md-8 col-sm-12">
-                @yield('content')
+        <div class="container fluid">
+            <div class="row fluid">
+                <div class="col-md-4 col-sm-12">
+                    @yield('navigation')
+                </div>
+                <div class="col-md-8 col-sm-12">
+                    @yield('content')
+                </div>
             </div>
         </div>
-
         <script type="text/javascript" src="{{ URL::asset('js/jquery-3.1.1.min.js') }}"></script>
         <script type="text/javascript" src="{{ URL::asset('js/bootstrap.min.js') }}"></script>
-        <script type="text/javascript" src="{{ URL::asset('js/npm.js') }}"></script>
 
     </body>
 </html>
