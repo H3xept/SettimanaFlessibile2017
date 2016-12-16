1a5e1af46cb6db6c0a2396e96f633e5770f687da
H3xept <h3xept@gmail.com>
2016-12-16 17:41:10 +0100

    [+] Init master

--- resources/views/partials/_header.blade.php
+++ resources/views/partials/_header.blade.php
@@ -1,7 +1,7 @@
 <div class="row">
 	<div class="col-md-4 col-md-offset-8">
 		<div align="right">
-		<h2>{{$name}}</h2><small>{{$class}}</small>
+		<h2>{{$name}}<small>{{$class}}</small></h2>
 		<button class="btn btn-danger">Logout</button>
 		</div>
 	</div>
