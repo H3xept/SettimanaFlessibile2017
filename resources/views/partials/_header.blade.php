3aaf949e78badad5dd316998f75f4ac6197e11c3
H3xept <h3xept@gmail.com>
2016-12-16 17:40:32 +0100

    [+] Init master

--- resources/views/partials/_header.blade.php
+++ resources/views/partials/_header.blade.php
@@ -1,7 +1,7 @@
 <div class="row">
 	<div class="col-md-4 col-md-offset-8">
 		<div align="right">
-		<h2>@yield('name')</h2><small>@yield('class')</small>
+		<h2>{{$name}}</h2><small>{{$class}}</small>
 		<button class="btn btn-danger">Logout</button>
 		</div>
 	</div>
