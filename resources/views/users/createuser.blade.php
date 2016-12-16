190dd5a8cf56c27bce19816f71d6b791b30b948f
H3xept <h3xept@gmail.com>
2016-12-16 16:36:44 +0100

    [*] Test user

---
+++ resources/views/users/createuser.blade.php
@@ -0,0 +1,6 @@
+<form action="/user" method="POST">
+	{{ csrf_field() }}
+	<input type="text" placeholder="Nome" name="name">
+	<input type="text" placeholder="Email" name="email">
+	<input type="text" placeholder="Password" name="password">
+</form>
