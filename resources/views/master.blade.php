fa9c3ee86215075808a2ba212b957c88ae44f4c4
H3xept <h3xept@gmail.com>
2016-12-16 18:24:47 +0100

    [*] Test structure

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -17,11 +17,9 @@
         <div class="row">
             <div class="col-md-4 col-sm-12">
                 @section('navigation')
-                @endsection
             </div>
             <div class="col-md-8 col-sm-12">
                 @section('content')
-                @endsection
             </div>
         </div>
 
