533bcef2e228278586f276fce0b510c1c98dede2
H3xept <h3xept@gmail.com>
2016-12-16 16:42:10 +0100

    [*] Test user

--- resources/views/users/createuser.blade.php
+++ resources/views/users/createuser.blade.php
@@ -3,4 +3,5 @@
 	<input type="text" placeholder="Nome" name="name">
 	<input type="text" placeholder="Email" name="email">
 	<input type="text" placeholder="Password" name="password">
+	<button type="submit">Mahunna</button>
 </form>
