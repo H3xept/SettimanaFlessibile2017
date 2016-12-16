ce27941281b7142c7ad12294b3721f9ba5579dd9
H3xept <h3xept@gmail.com>
2016-12-16 18:44:15 +0100

    [*] Contrast

--- resources/views/home/profile.blade.php
+++ resources/views/home/profile.blade.php
@@ -10,7 +10,7 @@ Profilo
 
 
 @section('content')
-<div id="list-id" class="jumbotron contrast" align="center">
+<div id="list-id" class="jumbotron" align="center" style="	border-color: #CCCCCC;border-width: 1px;border-style:solid;">
 	<div align="left"><h3>Programmazione settimana</h3></div><hr>
   <table class="table table-hover">
   <thead>
