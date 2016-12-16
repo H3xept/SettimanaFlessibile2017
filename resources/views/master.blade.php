984b21e621a24e1714a4536cd4fa4ddd9e0f784f
H3xept <h3xept@gmail.com>
2016-12-16 16:58:04 +0100

    [*] Test user

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -6,7 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Settimana Flessibile Galilei PG</title>
-        <link rel="stylesheet" type="text/css" href="../public/css/bootstrap.min.css"/>
+        <link rel="stylesheet" type="text/css" href="{{{ asset('/css/bootstrap.min.css') }}}"/>
 
     </head>
     <body>
