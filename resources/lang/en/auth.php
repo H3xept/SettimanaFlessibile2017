d5f84747d29930351062a753842c9f51aeba4e12
H3xept <h3xept@gmail.com>
2016-12-27 14:39:23 +0100

    [@] Ready for production 🍻

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed' => 'Nessun utente registrato con nome/password.',
+    'throttle' => 'Troppi tentativi di autenticazione, riprova tra :seconds secondi.',
 
 ];
