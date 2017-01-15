2bb7d8324452609cf2b31cce1464fbb31311cd32
H3xept <h3xept@gmail.com>
2017-01-16 00:17:11 +0100

    [...]

--- resources/views/home/courses.blade.php
+++ resources/views/home/courses.blade.php
@@ -214,7 +214,6 @@ $usr = Auth::user()->username;
 	}else{
     	var url = "/courses/{{$course->id}}/sign/";
 	}
-	console.log("MA DIOCANE");
     $.ajax({
            type: "POST",
            url: url,
